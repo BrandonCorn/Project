8cd7151378d01b6c9557648c523e9380670b4940
uchichabrandon <41881828+uchichabrandon@users.noreply.github.com>
2019-11-25 17:36:32 -0500

    js in own file now, small css changes

---
+++ ProjectJavaScript.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("SideNav").style.width = "250px";
+  document.getElementById("Main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "#D7CEC7";
+}
+function closeNav() {
+  document.getElementById("SideNav").style.width = "0";
+  document.getElementById("Main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "#D7CEC7";
+}
