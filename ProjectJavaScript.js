1da7b910d47dfc6a6fca330dcf96104166409890
uchichabrandon <41881828+uchichabrandon@users.noreply.github.com>
2019-12-04 02:46:08 -0500

    home page changes, working on backtotop btn

--- ProjectJavaScript.js
+++ ProjectJavaScript.js
@@ -8,3 +8,18 @@ function closeNav() {
   document.getElementById("Main").style.marginLeft = "0";
   document.body.style.backgroundColor = "#D7CEC7";
 }
+
+$(function() {
+
+  var $btn = $('#btnTop');
+  var $home = $('#TopSection');
+  var startpoint = $home.scrollTop() + $home.height();
+
+  $(window).on('scroll', function() {
+    if($(window).scrollTop() > startpoint) {
+      $btn.show();
+    } else {
+      $btn.hide();
+    }
+  });
+});
